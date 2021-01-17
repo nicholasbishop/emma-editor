4c3d9598bc3435676cd24ba184496aac52c09d68
Nicholas Bishop <nicholasbishop@gmail.com>
2021-01-17 14:36:42 -0500

    Initial commit

---
+++ src/bin/client.rs
@@ -0,0 +1,30 @@
+use gio::prelude::*;
+use gtk::prelude::*;
+
+use std::env;
+
+fn build_ui(application: &gtk::Application) {
+    let window = gtk::ApplicationWindow::new(application);
+
+    window.set_title("emma");
+    window.set_position(gtk::WindowPosition::Center);
+    window.set_default_size(350, 70);
+
+    let text = gtk::TextView::new();
+
+    window.add(&text);
+
+    window.show_all();
+}
+
+fn main() {
+    let application =
+        gtk::Application::new(Some("com.github.gtk-rs.examples.basic"), Default::default())
+            .expect("Initialization failed...");
+
+    application.connect_activate(|app| {
+        build_ui(app);
+    });
+
+    application.run(&env::args().collect::<Vec<_>>());
+}
