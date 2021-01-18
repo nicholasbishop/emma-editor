d9eeeb140f744b3fcf809542a678a70ad23b80f7
Nicholas Bishop <nicholasbishop@gmail.com>
2021-01-18 12:23:53 -0500

    Update embassy

--- src/bin/embassy.rs
+++ src/bin/embassy.rs
@@ -1,61 +1,74 @@
 use anyhow::Error;
+use crossbeam_channel::Receiver;
 use fehler::throws;
 use serde::{Deserialize, Serialize};
-use std::fs;
 use std::io::{self, Read, Write};
 use std::path::PathBuf;
+use std::{fs, thread};
 
 type Id = [u8; 8];
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Eq, PartialEq)]
 struct ReadFileRequest {
     id: Id,
     path: PathBuf,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Eq, PartialEq)]
 enum ReadFileResponseBody {
     TotalSize(usize),
     Data(Vec<u8>),
 }
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Eq, PartialEq)]
 struct ReadFileResponse {
     id: Id,
     body: ReadFileResponseBody,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Eq, PartialEq)]
 enum Request {
     Ping,
     ReadFile(ReadFileRequest),
+    Stop,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Eq, PartialEq)]
 enum Response {
     Pong,
     ReadFile(ReadFileResponse),
+    Stop,
 }
 
 #[throws]
-fn respond(resp: Response) {
-    // TODO for now just write to stdout and flush, may need to get
-    // more complicated eventually
-
-    let msg = bincode::serialize(&resp)?;
-
-    // TODO: could move this lock out
+fn response_thread(rx: Receiver<Response>) {
     let stdout = io::stdout();
     let mut stdout_handle = stdout.lock();
 
-    stdout_handle.write_all(&msg)?;
-    stdout_handle.flush()?;
+    loop {
+        let resp = rx.recv()?;
+        let msg = bincode::serialize(&resp)?;
+
+        stdout_handle.write_all(&msg)?;
+        stdout_handle.flush()?;
+
+        if resp == Response::Stop {
+            break;
+        }
+    }
 }
 
-fn main() -> Result<(), Error> {
+#[throws]
+fn main() {
     // TODO: split response work up into threads. Maybe one thread for
     // all file IO (or a file IO thread pool), one thread for each
     // long running process?
+
+    let (resp_tx, resp_rx) = crossbeam_channel::unbounded();
+
+    let respond = |resp| resp_tx.send(resp);
+
+    let response_thread_handle = thread::spawn(|| response_thread(resp_rx));
 
     let stdin = io::stdin();
     let mut stdin_handle = stdin.lock();
@@ -64,7 +77,6 @@ fn main() -> Result<(), Error> {
     loop {
         stdin_handle.read_exact(&mut len_buf)?;
         let msg_len = u16::from_le_bytes(len_buf);
-        dbg!(msg_len);
         msg_buf.resize(msg_len as usize, 0);
         stdin_handle.read_exact(&mut msg_buf)?;
 
@@ -84,6 +96,12 @@ fn main() -> Result<(), Error> {
                     }))?;
                 }
             }
+            Request::Stop => {
+                respond(Response::Stop)?;
+                break;
+            }
         }
     }
+
+    response_thread_handle.join().unwrap()?;
 }
