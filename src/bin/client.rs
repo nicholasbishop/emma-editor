f6c07dccd8c7c6e4abf3a8842a0529ca7b39cf0c
Nicholas Bishop <nicholasbishop@gmail.com>
2021-01-23 14:47:01 -0500

    client: add KeySequence parsing

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,8 +1,126 @@
 use gdk::keys::constants as keys;
+use gdk::{EventKey, ModifierType};
 use gio::prelude::*;
+use glib::translate::FromGlib;
 use gtk::prelude::*;
-
+use std::collections::HashMap;
 use std::env;
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+struct KeySequenceAtom {
+    modifiers: ModifierType,
+    key: gdk::keys::Key,
+}
+
+impl KeySequenceAtom {
+    fn from_event(e: &EventKey) -> KeySequenceAtom {
+        KeySequenceAtom {
+            modifiers: e.get_state(),
+            key: e.get_keyval(),
+        }
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+struct KeySequence(Vec<KeySequenceAtom>);
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+enum KeySequenceParseError {
+    InvalidEscape(char),
+    InvalidName(String),
+}
+
+impl KeySequence {
+    // TODO: change to a Result
+    fn parse(s: &str) -> Result<KeySequence, KeySequenceParseError> {
+        enum State {
+            Initial,
+            InName,
+            InEscape,
+        }
+
+        #[derive(Clone)]
+        enum Item {
+            Modifier(ModifierType),
+            Key(gdk::keys::Key),
+        }
+
+        let mut state = State::Initial;
+
+        let mut names = HashMap::new();
+        names.insert("ctrl", Item::Modifier(ModifierType::CONTROL_MASK));
+        names.insert("shift", Item::Modifier(ModifierType::SHIFT_MASK));
+        names.insert("alt", Item::Modifier(ModifierType::MOD1_MASK));
+        names.insert("esc", Item::Key(keys::Escape));
+        names.insert("space", Item::Key(keys::space));
+
+        let mut items = Vec::new();
+        let mut name = String::new();
+        for c in s.chars() {
+            match state {
+                State::Initial => {
+                    if c == '\\' {
+                        state = State::InEscape;
+                    } else if c == '<' {
+                        state = State::InName;
+                    } else if c == '+' {
+                        // Nothing to do, this is just an explicit way
+                        // to break up the key sequence
+                    } else {
+                        let keyval = gdk::unicode_to_keyval(c as u32);
+                        items.push(Item::Key(gdk::keys::Key::from_glib(keyval)))
+                    }
+                }
+                State::InEscape => {
+                    if c == '<' {
+                        items.push(Item::Key(keys::leftanglebracket));
+                    } else if c == '>' {
+                        items.push(Item::Key(keys::rightanglebracket));
+                    } else if c == '\\' {
+                        items.push(Item::Key(keys::backslash));
+                    } else if c == '+' {
+                        items.push(Item::Key(keys::plus));
+                    } else {
+                        return Err(KeySequenceParseError::InvalidEscape(c));
+                    }
+                    state = State::Initial;
+                }
+                State::InName => {
+                    if c == '>' {
+                        if let Some(val) = names.get(name.as_str()) {
+                            items.push(val.clone());
+                        } else {
+                            return Err(KeySequenceParseError::InvalidName(
+                                name,
+                            ));
+                        }
+                        state = State::Initial;
+                    } else {
+                        name.push(c);
+                    }
+                }
+            }
+        }
+
+        let mut seq = Vec::new();
+        let mut cur_mods = ModifierType::empty();
+
+        for item in items {
+            match item {
+                Item::Modifier(m) => cur_mods |= m,
+                Item::Key(k) => {
+                    seq.push(KeySequenceAtom {
+                        modifiers: cur_mods,
+                        key: k,
+                    });
+                    cur_mods = ModifierType::empty();
+                }
+            }
+        }
+
+        Ok(KeySequence(seq))
+    }
+}
 
 fn build_ui(application: &gtk::Application) {
     let window = gtk::ApplicationWindow::new(application);
@@ -17,13 +135,18 @@ fn build_ui(application: &gtk::Application) {
 
     window.add_events(gdk::EventMask::KEY_PRESS_MASK);
     window.connect_key_press_event(|_, e| {
+        // Ignore lone modifier presses.
         if e.get_is_modifier() {
-            Inhibit(false)
-        } else if e.get_keyval() == keys::Escape {
+            return Inhibit(false);
+        }
+
+        let atom = KeySequenceAtom::from_event(e);
+
+        if atom.key == keys::Escape {
             // TODO: for now make it easy to quit
             std::process::exit(0);
-        } else if e.get_state() == gdk::ModifierType::CONTROL_MASK
-            && e.get_keyval() == keys::f
+        } else if atom.modifiers == gdk::ModifierType::CONTROL_MASK
+            && atom.key == keys::f
         {
             dbg!("C-f");
             Inhibit(true)
@@ -47,4 +170,21 @@ fn main() {
     });
 
     application.run(&env::args().collect::<Vec<_>>());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sequence_parse() {
+        gdk::init();
+        assert_eq!(
+            KeySequence::parse("f"),
+            Ok(KeySequence(vec![KeySequenceAtom {
+                modifiers: ModifierType::empty(),
+                key: keys::f,
+            }]))
+        );
+    }
 }
