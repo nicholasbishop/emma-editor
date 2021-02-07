2c20d4e463bafda668b0debce6d5dda3eaae77cb
Nicholas Bishop <nicholasbishop@gmail.com>
2021-02-07 15:40:37 -0500

    First pass at adding info bar

--- src/bin/client/pane.rs
+++ src/bin/client/pane.rs
@@ -4,6 +4,10 @@ type View = gtk::TextView;
 
 #[derive(Clone, Eq, PartialEq)]
 pub struct Pane {
+    container: gtk::Box,
+
+    info: gtk::Label,
+
     scrolled_window: gtk::ScrolledWindow,
     view: View,
 }
@@ -15,14 +19,29 @@ impl Pane {
         let adj: Option<&gtk::Adjustment> = None;
         let scrolled_window = gtk::ScrolledWindow::new(adj, adj);
         scrolled_window.add(&view);
+
+        let info = gtk::Label::new(Some("TODO"));
+        info.set_widget_name("info");
+        info.set_xalign(0.0);
+
+        let container = gtk::Box::new(gtk::Orientation::Vertical, 0);
+        let expand = true;
+        let fill = true;
+        let padding = 0;
+        container.pack_start(&scrolled_window, expand, fill, padding);
+        let expand = false;
+        container.pack_start(&info, expand, fill, padding);
+
         Pane {
+            container,
+            info,
             scrolled_window,
             view,
         }
     }
 
     pub fn get_widget(&self) -> gtk::Widget {
-        self.scrolled_window.clone().upcast()
+        self.container.clone().upcast()
     }
 
     // TODO: might want to remove this and just stick to a concept of
