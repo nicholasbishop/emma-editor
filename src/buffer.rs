27b4e05a72ecd27aa504a515f85ea1cece179065
Nicholas Bishop <nicholasbishop@gmail.com>
2021-03-27 16:43:42 -0400

    Stub in some buffer code

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,5 +1,28 @@
-use ropey::Rope;
+use {
+    anyhow::Error,
+    fehler::throws,
+    ropey::Rope,
+    std::{
+        fs, io,
+        path::{Path, PathBuf},
+    },
+};
 
 pub struct Buffer {
     text: Rope,
+    path: PathBuf,
+}
+
+impl Buffer {
+    #[throws]
+    pub fn from_path(path: &Path) -> Buffer {
+        let text =
+            Rope::from_reader(&mut io::BufReader::new(fs::File::open(path)?))?;
+        let mut buffer = Buffer {
+            text,
+            path: path.into(),
+        };
+
+        buffer
+    }
 }
