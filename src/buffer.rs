9362ff91f49f8ab20731181e242d694a9b6c78da
Nicholas Bishop <nicholasbishop@gmail.com>
2021-03-27 22:24:18 -0400

    Add BufferId

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,6 +1,7 @@
 use {
     anyhow::Error,
     fehler::throws,
+    rand::{distributions::Alphanumeric, thread_rng, Rng},
     ropey::Rope,
     std::{
         fs, io,
@@ -8,7 +9,22 @@ use {
     },
 };
 
+pub struct BufferId(String);
+
+impl BufferId {
+    fn new() -> BufferId {
+        let r: String = thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(8)
+            .map(char::from)
+            .collect();
+        BufferId(format!("buffer-{}", r))
+    }
+}
+
 pub struct Buffer {
+    id: BufferId,
+
     text: Rope,
     path: PathBuf,
 }
@@ -18,11 +34,10 @@ impl Buffer {
     pub fn from_path(path: &Path) -> Buffer {
         let text =
             Rope::from_reader(&mut io::BufReader::new(fs::File::open(path)?))?;
-        let buffer = Buffer {
+        Buffer {
+            id: BufferId::new(),
             text,
             path: path.into(),
-        };
-
-        buffer
+        }
     }
 }
