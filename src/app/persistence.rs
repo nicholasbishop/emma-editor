ebef8eb5bb717edfe98d51ab8f806eda1ed72b75
Nicholas Bishop <nbishop@nbishop.net>
2022-07-19 13:41:22 -0400

    Implement more of persistence with sqlite

--- src/app/persistence.rs
+++ src/app/persistence.rs
@@ -1,10 +1,53 @@
 use super::App;
-use anyhow::Result;
+use anyhow::{anyhow, Result};
+use fs_err as fs;
+use rusqlite::Connection;
+use std::path::PathBuf;
+
+/// Name of the sqlite3 database file used to store cached data. This
+/// data is separate from the config data (see config.rs). It can be
+/// safely deleted, you'll just lose things like the list of open
+/// buffers and pane tree layout after restarting the app.
+const DB_NAME: &str = "emma.sqlite3";
+
+/// Get the cache directory path, e.g. "~/.cache/emma".
+fn cache_dir() -> Result<PathBuf> {
+    Ok(dirs::cache_dir()
+        .ok_or_else(|| anyhow!("cache dir unknown"))?
+        .join("emma"))
+}
 
 impl App {
     pub fn persistence_store(&self) -> Result<()> {
+        let cache_dir = cache_dir()?;
+
+        // Try to create the directory. Ignore the error, it might
+        // already exist.
+        let _ = fs::create_dir_all(&cache_dir);
+
+        // Open the database, creating it if necessary.
+        let db_path = cache_dir.join(DB_NAME);
+        let conn = Connection::open(db_path)?;
+
+        // Create the tables if not already present.
+        conn.execute(
+            "CREATE TABLE IF NOT EXISTS kv (
+                key   TEXT PRIMARY KEY,
+                value TEXT
+            )",
+            (),
+        )?;
+
         let json = serde_json::to_string(&self.pane_tree)?;
-        dbg!(json);
+        conn.execute(
+            "INSERT INTO kv (key, value) VALUES (?1, ?2)",
+            ("pane_tree", &json),
+        )?;
+
         Ok(())
+    }
+
+    pub fn persistence_load(&mut self) -> Result<()> {
+        todo!()
     }
 }
