0b0cd9b0092384929818ba9a777054c1e7d12c4a
Nicholas Bishop <nbishop@nbishop.net>
2022-07-24 16:04:55 -0400

    Stub in some more of shell

--- src/shell.rs
+++ src/shell.rs
@@ -1,1 +1,11 @@
-pub struct Shell {}
+use anyhow::Result;
+use nix::pty::PtyMaster;
+
+pub struct Shell {
+    master: PtyMaster,
+}
+impl Shell {
+    pub fn new() -> Result<Shell> {
+        todo!();
+    }
+}
